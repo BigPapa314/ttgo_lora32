4ac06fc3b8f353c35b774ed06022e15032c5da6d
Thomas Kilian <thomas-kilian@gmx.net>
2025-06-09 13:46:18 +0200

    add ssd1306 projects

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # TTGO LORA32 projects
 
 All projects starting with "ttgo_" are projects that can be deployd on a ttgo_lora32 v1.6.1 module.
+
+# peripherals
+
+* Radio: https://docs.rs/sx127x_lora/latest/sx127x_lora/
+* Display: https://docs.rs/ssd1306/latest/ssd1306/
---
+++ ttgo_ssd1306/src/main.rs
@@ -0,0 +1,22 @@
+use esp_idf_svc::hal::delay::FreeRtos;
+use esp_idf_svc::hal::gpio::*;
+use esp_idf_svc::hal::peripherals::Peripherals;
+use ssd1306::Blinky;
+
+fn main() {
+    // It is necessary to call this function once. Otherwise some patches to the runtime
+    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
+    esp_idf_svc::sys::link_patches();
+
+    // Bind the log crate to the ESP Logging facilities
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    log::info!("Hello, world!");
+
+    let peripherals = Peripherals::take().unwrap();
+    let led = PinDriver::output(peripherals.pins.gpio25).unwrap();
+
+    let blinky = Blinky::new(FreeRtos, led);
+
+    blinky.run();
+}
