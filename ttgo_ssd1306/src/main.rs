e0c604bd790e26d247c676789f592e0115dd7166
Thomas Kilian <thomas-kilian@gmx.net>
2025-06-11 19:26:36 +0200

    Implemeted ttgo_ssd1306 hello world

--- ttgo_ssd1306/src/main.rs
+++ ttgo_ssd1306/src/main.rs
@@ -1,7 +1,64 @@
 use esp_idf_svc::hal::delay::FreeRtos;
 use esp_idf_svc::hal::gpio::*;
+use esp_idf_svc::hal::i2c::*;
 use esp_idf_svc::hal::peripherals::Peripherals;
-use ssd1306::Blinky;
+use esp_idf_svc::hal::prelude::*;
+use esp_idf_svc::hal::units::Hertz;
+
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    pixelcolor::Rgb565,
+    prelude::*,
+    primitives::{Circle, PrimitiveStyle, PrimitiveStyleBuilder},
+    text::{Baseline, Text},
+};
+use ssd1306::{mode::BufferedGraphicsMode, prelude::*, I2CDisplayInterface, Ssd1306};
+
+use ssd1306::mode::TerminalDisplaySize;
+use ssd1306::mode::TerminalMode;
+use std::fmt::Write;
+
+struct Ttgo32LoraDisplay<DI, SIZE, TDelay>
+where
+    TDelay: embedded_hal::delay::DelayNs,
+{
+    driver: Ssd1306<DI, SIZE, TerminalMode>,
+    delay: TDelay,
+}
+
+impl<DI, SIZE, TDelay> Ttgo32LoraDisplay<DI, SIZE, TDelay>
+where
+    TDelay: embedded_hal::delay::DelayNs,
+{
+    fn new(driver: Ssd1306<DI, SIZE, TerminalMode>, delay: TDelay) -> Self {
+        Self { driver, delay }
+    }
+
+    fn driver(&mut self) -> &mut Ssd1306<DI, SIZE, TerminalMode> {
+        &mut self.driver
+    }
+}
+
+impl<DI, SIZE, TDelay> std::fmt::Write for Ttgo32LoraDisplay<DI, SIZE, TDelay>
+where
+    DI: WriteOnlyDataCommand,
+    SIZE: TerminalDisplaySize,
+    TDelay: embedded_hal::delay::DelayNs,
+{
+    fn write_str(&mut self, s: &str) -> Result<(), std::fmt::Error> {
+        s.chars().for_each(|c| {
+            self.driver().print_char(c);
+        });
+
+        // for c in s.chars() {
+        //     self.driver().print_char(c);
+        //     // self.delay.delay_ms(1);
+        // }
+
+        Ok(())
+    }
+}
 
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -11,12 +68,28 @@ fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
+    log::info!("Hello, ssd1306!");
 
     let peripherals = Peripherals::take().unwrap();
-    let led = PinDriver::output(peripherals.pins.gpio25).unwrap();
 
-    let blinky = Blinky::new(FreeRtos, led);
+    let i2c0 = peripherals.i2c0;
 
-    blinky.run();
+    let sda = peripherals.pins.gpio21;
+    let scl = peripherals.pins.gpio22;
+
+    let display_i2c_config = I2cConfig::new().baudrate(400.kHz().into());
+    let display_i2c = I2cDriver::new(i2c0, sda, scl, &display_i2c_config).unwrap();
+
+    let interface = I2CDisplayInterface::new(display_i2c);
+
+    let mut display_driver =
+        Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0).into_terminal_mode();
+
+    let mut display = Ttgo32LoraDisplay::new(display_driver, FreeRtos);
+
+    display.driver().init().unwrap();
+    display.driver().clear().unwrap();
+
+    write!(display, "Hello World!");
+    FreeRtos::delay_ms(1);
 }
