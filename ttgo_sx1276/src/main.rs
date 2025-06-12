bacefd408ba06d3c37457e7dfa3a66733db607f3
Thomas Kilian <thomas-kilian@gmx.net>
2025-06-12 13:44:43 +0200

    Added ttgo_sx1276

--- ttgo_sx1276/src/main.rs
+++ ttgo_sx1276/src/main.rs
@@ -1,20 +1,38 @@
 use esp_idf_svc::hal::delay::FreeRtos;
+use esp_idf_svc::hal::gpio::*;
 use esp_idf_svc::hal::i2c::*;
 use esp_idf_svc::hal::peripherals::Peripherals;
 use esp_idf_svc::hal::prelude::*;
+use esp_idf_svc::hal::units::Hertz;
 
-use ssd1306::{mode::TerminalMode, prelude::*, I2CDisplayInterface, Ssd1306};
+use embedded_graphics::{
+    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+    pixelcolor::BinaryColor,
+    pixelcolor::Rgb565,
+    prelude::*,
+    primitives::{Circle, PrimitiveStyle, PrimitiveStyleBuilder},
+    text::{Baseline, Text},
+};
+use ssd1306::{mode::BufferedGraphicsMode, prelude::*, I2CDisplayInterface, Ssd1306};
 
 use ssd1306::mode::TerminalDisplaySize;
+use ssd1306::mode::TerminalMode;
 use std::fmt::Write;
 
-struct Ttgo32LoraDisplay<DI, SIZE> {
+struct Ttgo32LoraDisplay<DI, SIZE, TDelay>
+where
+    TDelay: embedded_hal::delay::DelayNs,
+{
     driver: Ssd1306<DI, SIZE, TerminalMode>,
+    delay: TDelay,
 }
 
-impl<DI, SIZE> Ttgo32LoraDisplay<DI, SIZE> {
-    fn new(driver: Ssd1306<DI, SIZE, TerminalMode>) -> Self {
-        Self { driver }
+impl<DI, SIZE, TDelay> Ttgo32LoraDisplay<DI, SIZE, TDelay>
+where
+    TDelay: embedded_hal::delay::DelayNs,
+{
+    fn new(driver: Ssd1306<DI, SIZE, TerminalMode>, delay: TDelay) -> Self {
+        Self { driver, delay }
     }
 
     fn driver(&mut self) -> &mut Ssd1306<DI, SIZE, TerminalMode> {
@@ -22,15 +40,23 @@ impl<DI, SIZE> Ttgo32LoraDisplay<DI, SIZE> {
     }
 }
 
-impl<DI, SIZE> std::fmt::Write for Ttgo32LoraDisplay<DI, SIZE>
+impl<DI, SIZE, TDelay> std::fmt::Write for Ttgo32LoraDisplay<DI, SIZE, TDelay>
 where
     DI: WriteOnlyDataCommand,
     SIZE: TerminalDisplaySize,
+    TDelay: embedded_hal::delay::DelayNs,
 {
     fn write_str(&mut self, s: &str) -> Result<(), std::fmt::Error> {
-        s.chars()
-            .try_for_each(|c| self.driver().print_char(c))
-            .map_err(|_| std::fmt::Error)
+        s.chars().for_each(|c| {
+            self.driver().print_char(c);
+        });
+
+        // for c in s.chars() {
+        //     self.driver().print_char(c);
+        //     // self.delay.delay_ms(1);
+        // }
+
+        Ok(())
     }
 }
 
@@ -56,18 +82,14 @@ fn main() {
 
     let interface = I2CDisplayInterface::new(display_i2c);
 
-    let display_driver =
+    let mut display_driver =
         Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0).into_terminal_mode();
 
-    let mut display = Ttgo32LoraDisplay::new(display_driver);
+    let mut display = Ttgo32LoraDisplay::new(display_driver, FreeRtos);
 
     display.driver().init().unwrap();
     display.driver().clear().unwrap();
 
-    let mut counter = 0;
-    loop {
-        writeln!(display, "Hello World: {counter}").unwrap();
-        counter += 1;
-        FreeRtos::delay_ms(500);
-    }
+    write!(display, "Hello World!");
+    FreeRtos::delay_ms(1);
 }
