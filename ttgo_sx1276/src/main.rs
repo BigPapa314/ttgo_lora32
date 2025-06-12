9f74cf02cef3fbcef0e507d819d1dd66c16881d9
Thomas Kilian <thomas-kilian@gmx.net>
2025-06-12 17:24:43 +0200

    Added sx127x_rs

--- ttgo_sx1276/src/main.rs
+++ ttgo_sx1276/src/main.rs
@@ -1,64 +1,8 @@
 use esp_idf_svc::hal::delay::FreeRtos;
 use esp_idf_svc::hal::gpio::*;
-use esp_idf_svc::hal::i2c::*;
 use esp_idf_svc::hal::peripherals::Peripherals;
 use esp_idf_svc::hal::prelude::*;
-use esp_idf_svc::hal::units::Hertz;
-
-use embedded_graphics::{
-    mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
-    pixelcolor::BinaryColor,
-    pixelcolor::Rgb565,
-    prelude::*,
-    primitives::{Circle, PrimitiveStyle, PrimitiveStyleBuilder},
-    text::{Baseline, Text},
-};
-use ssd1306::{mode::BufferedGraphicsMode, prelude::*, I2CDisplayInterface, Ssd1306};
-
-use ssd1306::mode::TerminalDisplaySize;
-use ssd1306::mode::TerminalMode;
-use std::fmt::Write;
-
-struct Ttgo32LoraDisplay<DI, SIZE, TDelay>
-where
-    TDelay: embedded_hal::delay::DelayNs,
-{
-    driver: Ssd1306<DI, SIZE, TerminalMode>,
-    delay: TDelay,
-}
-
-impl<DI, SIZE, TDelay> Ttgo32LoraDisplay<DI, SIZE, TDelay>
-where
-    TDelay: embedded_hal::delay::DelayNs,
-{
-    fn new(driver: Ssd1306<DI, SIZE, TerminalMode>, delay: TDelay) -> Self {
-        Self { driver, delay }
-    }
-
-    fn driver(&mut self) -> &mut Ssd1306<DI, SIZE, TerminalMode> {
-        &mut self.driver
-    }
-}
-
-impl<DI, SIZE, TDelay> std::fmt::Write for Ttgo32LoraDisplay<DI, SIZE, TDelay>
-where
-    DI: WriteOnlyDataCommand,
-    SIZE: TerminalDisplaySize,
-    TDelay: embedded_hal::delay::DelayNs,
-{
-    fn write_str(&mut self, s: &str) -> Result<(), std::fmt::Error> {
-        s.chars().for_each(|c| {
-            self.driver().print_char(c);
-        });
-
-        // for c in s.chars() {
-        //     self.driver().print_char(c);
-        //     // self.delay.delay_ms(1);
-        // }
-
-        Ok(())
-    }
-}
+use esp_idf_svc::hal::spi::*;
 
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -68,28 +12,35 @@ fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Hello, ssd1306!");
+    log::info!("Hello, sx127x_lora!");
 
     let peripherals = Peripherals::take().unwrap();
 
-    let i2c0 = peripherals.i2c0;
+    let spi2 = peripherals.spi2;
 
-    let sda = peripherals.pins.gpio21;
-    let scl = peripherals.pins.gpio22;
+    let sclk = peripherals.pins.gpio5;
+    let miso = peripherals.pins.gpio19;
+    let mosi = peripherals.pins.gpio27;
+    let cs = peripherals.pins.gpio18;
+    let rst = peripherals.pins.gpio23;
+    // let dio = peripherals.pins.gpio26;
 
-    let display_i2c_config = I2cConfig::new().baudrate(400.kHz().into());
-    let display_i2c = I2cDriver::new(i2c0, sda, scl, &display_i2c_config).unwrap();
+    log::info!("Starting SPI loopback test");
+    // let spi_driver_config = SpiDriverConfig::new().baudrate(18.MHz().into());
+    let spi_driver_config = SpiDriverConfig::new();
+    let spi_driver = SpiDriver::new(spi2, sclk, miso, Some(mosi), &spi_driver_config).unwrap();
+    let spi_config = SpiConfig::new().baudrate(18.MHz().into());
+    let spi = SpiBusDriver::new(spi_driver, &spi_config).unwrap();
+    log::info!("spistuff");
 
-    let interface = I2CDisplayInterface::new(display_i2c);
+    let mut lora = sx127x_lora::LoRa::new(
+        spi,
+        PinDriver::input_output(cs).unwrap(),
+        PinDriver::input_output(rst).unwrap(),
+        915_000_000,
+        FreeRtos,
+    )
+    .unwrap();
 
-    let mut display_driver =
-        Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0).into_terminal_mode();
-
-    let mut display = Ttgo32LoraDisplay::new(display_driver, FreeRtos);
-
-    display.driver().init().unwrap();
-    display.driver().clear().unwrap();
-
-    write!(display, "Hello World!");
-    FreeRtos::delay_ms(1);
+    log::info!("sx127x_lora has been initialized");
 }
