39ee80e8152f8235c96ae302389118780ac5070a
Thomas Kilian <thomas-kilian@gmx.net>
2025-06-24 14:30:00 +0200

    Made lora protocol optional

--- sx127x/sx127x_rs/src/lib.rs
+++ sx127x/sx127x_rs/src/lib.rs
@@ -1,8 +1,16 @@
 pub use sx127x_rs_driver as driver;
+
+#[cfg(feature = "lora")]
 pub use sx127x_rs_lora as lora;
 
 pub mod prelude {
     pub use crate::driver::Sx127xDriver;
+    #[cfg(feature = "lora")]
+    pub use crate::prelude_lora::*;
+}
+
+#[cfg(feature = "lora")]
+mod prelude_lora {
     pub use crate::lora::Sx127xLora;
     pub use crate::lora::Sx127xLoraExt;
 }
